28794963e733bcc60c292c52d8a15ae1fe372e6d
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-04-18 13:58:47 +0100

    first generator
    simple number generator

---
+++ js/lotto.js
@@ -0,0 +1,19 @@
+// lotto number generator
+
+function myLotto() {
+	var numbers = [];
+	while (numbers.length < 6) {
+		var random = Math.floor(Math.random() * 49) + 1;
+		
+		if (numbers.indexOf(random) == -1) {
+			numbers.push(random);
+		}
+	}
+	
+	
+	numbers.sort(function (a,b) {
+		return a - b;
+	});
+	
+	document.getElementById("lottoNumbers").innerHTML = numbers.join(",");
+}
